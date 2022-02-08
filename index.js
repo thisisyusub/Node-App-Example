f92053d7c8c08afdf3d054e59d1b70cad4a4b477
Thisisyusub <kanan.yusub@gmail.com>
2022-02-08 23:22:53 +0400

    changed

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const morgan = require('morgan');
+const config = require('config');
+
 const app = express();
 
-app.get('/', (req, res, next) => {
-    res.send('Hello World!');
-});
+console.log(`Application Name: ${config.get('name')}`);
+
+// middlewares
+if (app.get('env') === 'development') {
+    app.use(morgan('tiny'));
+    console.log('Morgan enabled.!');
+}
+
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
